5a602997ac0fee6568cd8db8e35513a163f81d53
ucabaa9 <35300615+ucabaa9@users.noreply.github.com>
2021-02-21 12:02:47 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDC_Code_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Removing duplicates characters
+            string replchar = ("AAAc91%cWwWkLq$1ci3_848v3d_K");
+            string newString = string.Join("", replchar.ToCharArray().Distinct());
+            Console.WriteLine(newString);
+
+            //Replacing the $ sign with £ sign
+            string newstr = newString.Replace('$', '£');
+            Console.WriteLine(newstr);
+
+            //Removing  '4' and '_' from the the string
+            string newstr1 = String.Join("", newstr.Split('4', '_'));
+            Console.WriteLine(newstr1);
+
+            //Truncating the length of the string to 15 characters
+            const int MaxLength = 15;
+            string newstr2 = newstr1;
+                if (newstr2.Length > MaxLength)
+                    newstr2 = newstr2.Substring(0, MaxLength);
+                Console.WriteLine(newstr2);
+            Console.Read();
+
+        }
+    }
+}
